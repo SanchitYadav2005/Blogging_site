a9fe644684cfe3b73c01031209b86fe7acf3eed7
unknown <sanchit2005yudavanshi@gmail.com>
2022-12-27 19:40:35 +0530

    serverd static folder in the server.js file

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req,res)=>{
     res.send("server is working");
