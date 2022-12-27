71e42e91ddac0a8045d3711821ec0bb0322af5bc
unknown <sanchit2005yudavanshi@gmail.com>
2022-12-27 19:28:30 +0530

    created views folder

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.use
+
 app.get('/', (req,res)=>{
     res.send("server is working");
 });
+
+
 app.listen(port, function(){
     console.log(`listining on the port :- ${port}!`);
 });
