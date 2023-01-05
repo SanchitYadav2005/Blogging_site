beb6cba5334eedd20a693c7b08bbb8f7d7d81784
unknown <sanchit2005yudavanshi@gmail.com>
2023-01-05 21:19:58 +0530

    ejs-mate started working || some filenaming issue is rised

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req,res)=>{
-    res.render('home');
+    res.render('pages/home');
 });
 
 
