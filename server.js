fe4b8b4bf3f1ef33046e8e1d0dc20c4c25392db1
unknown <sanchit2005yudavanshi@gmail.com>
2023-02-03 02:50:55 +0530

    adding method overriding in the script

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const ejsMate = require('ejs-mate');
 const port = 8080;
 const path = require('path');
+const methodOverride = require('method-override');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(methodOverride("_method"));
 
 app.get('/', (req,res)=>{
     res.render('pages/home');
