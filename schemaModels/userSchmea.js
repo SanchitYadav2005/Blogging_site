02f57983ee0149ed48c3810384f71a59361e2856
unknown <sanchit2005yudavanshi@gmail.com>
2023-02-11 20:40:29 +0530

    created the schema for the user with new key pair values

--- schemaModels/userSchmea.js
+++ schemaModels/userSchmea.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
-    name:String,
-    email:String
+    name:{
+        fname: String,
+        lname: String
+    },
+    email:String,
+    password:String,
+    mobile:Number,
+    country: String,
+    city:String
 });
 
 module.exports = mongoose.model('User', UserSchema);
