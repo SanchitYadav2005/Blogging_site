09045870069e9f415d9514ccacc4697e7713ef09
unknown <sanchit2005yudavanshi@gmail.com>
2023-02-17 20:55:47 +0530

    added cloudinary as the storage option in the multer destination

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const users = require('../controllers/userControls');
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const {storage} = require('../cloudinary/index');
+const multer  = require('multer');
+const upload = multer({storage})
 
 router.route('/')
       .get(users.homePage)
 
 router.route('/createAccount')
      .get(users.createAccount)
-     .post(upload.single('image'), (req,res)=>{
-            console.log(req.body,req.file);
-            res.send("It worked");
-     })
+     .post(upload.single('image'), users.uploadAccount);
 
 router.get('/:id/profile', users.showProfile)
 
