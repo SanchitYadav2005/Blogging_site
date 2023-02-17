e1d5efbeccf57526e56f62e8d8be5342b6aa035b
unknown <sanchit2005yudavanshi@gmail.com>
2023-02-17 20:39:26 +0530

    multer is setedup but req.file is sending undefined

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const users = require('../controllers/userControls');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
       .get(users.homePage)
 
 router.route('/createAccount')
      .get(users.createAccount)
-     .post(users.uploadAccount)
+     .post(upload.single('image'), (req,res)=>{
+            console.log(req.body,req.file);
+            res.send("It worked");
+     })
 
 router.get('/:id/profile', users.showProfile)
 
